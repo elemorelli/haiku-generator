fa4ca6d93895dc8bcec487bb9943d5899ac89da2
Ele Morelli <etc+github@elemorelli.com>
2023-11-10 18:59:13 -0300

    Clean dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,12 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import alpinejs from "@astrojs/alpinejs";
-
-// https://astro.build/config
-import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), alpinejs(), mdx()],
+  integrations: [tailwind()],
   output: "server",
   adapter: netlify(),
 });
